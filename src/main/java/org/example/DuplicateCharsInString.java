73c01ff1ae94d607c89ad07348111ea90ae7b49c
TanmoyAsset <1792976@tcs.com>
2023-11-23 17:49:09 +0530

    first commit

---
+++ src/main/java/org/example/DuplicateCharsInString.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class DuplicateCharsInString {
+
+    public static void duplicateCharsInString(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String str=sc.nextLine();
+
+
+
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        ReverseString.reverseString();
+//        Swap2NosUsing3rdVariable.swap2NosUsing3rdVariable();
+//        Swap2NosWithout3rdVariable.swap2NosWithout3rdVariable();
+//        PrimeNo.primeNo();
+//        Fibonacci.fibonacci();
+        HashMapPractice.hashMapPractice();
+
+    }
+
+}
